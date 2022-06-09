183a84b62491d3670128f9da680f47b8699a72fa
DrKleiber <wellliu127@gmail.com>
2022-06-09 17:47:50 -0500

    Create submit_dist_2parts_test.sh

---
+++ submit_dist_2parts_test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#SBATCH --job-name=DT_HTG_2parts
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:6
+#SBATCH --time=72:00:00
+
+# srun python ./train_MeanStd.py > test.log
+# export PATH=/soft/swing/pytorch/1.10/cuda-11.3/bin:$PATH
+srun torchrun HTGNN_train_dist_2parts.py > powerDrop_dist_2parts_test.log
