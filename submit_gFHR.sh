20e5e31cea679d67e6db0288b2dcbc4101d025b4
DrKleiber <wellliu127@gmail.com>
2022-09-16 15:28:55 -0500

    1

--- submit_gFHR.sh
+++ submit_gFHR.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=DT_HTG
+#SBATCH --job-name=DT_gFHR
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:1
 #SBATCH --time=24:00:00
 
 # srun python ./train_MeanStd.py > test.log
 # export PATH=/soft/swing/pytorch/1.10/cuda-11.3/bin:$PATH
-srun python ./HTGNN_train.py > powerDrop_01.log
+srun python ./gFHR_train_2parts.py > gFHR_test.log
